1898c4211b3155bcf8e7e92b89cc8c2f56f7b728
Eric3337 <ezhang3337@gmail.com>
2024-11-10 12:45:27 -0800

    Added in Figma to React

---
+++ frontend/src/components/WithoutResume/WithoutResume.jsx
@@ -0,0 +1,273 @@
+import React from 'react';
+
+import PngtreeFileUploadIcoImage from 'src/assets/images/WithoutResume___Pngtree_file_upload_icon_4646955_1.png';
+
+import {
+  styled
+} from '@mui/material/styles';
+
+const WithoutResume1 = styled("div")({
+  backgroundColor: `rgba(255, 255, 255, 1)`,
+  display: `flex`,
+  position: `relative`,
+  isolation: `isolate`,
+  flexDirection: `row`,
+  width: `1440px`,
+  height: `898px`,
+  justifyContent: `flex-start`,
+  alignItems: `flex-start`,
+  padding: `0px`,
+  boxSizing: `border-box`,
+  overflow: `hidden`,
+});
+
+const Background = styled("div")({
+  backgroundColor: `rgba(4, 36, 68, 1)`,
+  width: `722px`,
+  height: `898px`,
+  position: `absolute`,
+  left: `718px`,
+  top: `0px`,
+});
+
+const Frame5 = styled("div")({
+  display: `flex`,
+  position: `absolute`,
+  isolation: `isolate`,
+  flexDirection: `column`,
+  justifyContent: `flex-start`,
+  alignItems: `center`,
+  padding: `0px`,
+  boxSizing: `border-box`,
+  width: `656px`,
+  height: `796px`,
+  left: `751px`,
+  top: `63px`,
+});
+
+const Scopify = styled("div")({
+  textAlign: `center`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(255, 255, 255, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Inter`,
+  fontWeight: `400`,
+  fontSize: `64px`,
+  letterSpacing: `0px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  alignSelf: `stretch`,
+  margin: `0px`,
+});
+
+const Group2 = styled("div")({
+  boxShadow: `0px 4px 4px rgba(0, 0, 0, 0.25)`,
+  display: `flex`,
+  position: `relative`,
+  isolation: `isolate`,
+  flexDirection: `row`,
+  justifyContent: `flex-start`,
+  alignItems: `flex-start`,
+  padding: `0px`,
+  boxSizing: `border-box`,
+  height: `162px`,
+  width: `562px`,
+  margin: `31px 0px 0px 0px`,
+});
+
+const Frame2 = styled("div")({
+  backgroundColor: `rgba(204, 204, 204, 1)`,
+  border: `5px solid rgba(0, 0, 0, 1)`,
+  boxSizing: `border-box`,
+  display: `flex`,
+  position: `absolute`,
+  isolation: `isolate`,
+  flexDirection: `row`,
+  justifyContent: `flex-start`,
+  alignItems: `flex-start`,
+  padding: `0px`,
+  width: `562px`,
+  height: `162px`,
+  left: `0px`,
+  top: `0px`,
+  overflow: `hidden`,
+});
+
+const ChooseAFileOrDragItHSpan1 = styled("span")({
+  whiteSpace: `pre-wrap`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Inter`,
+  fontWeight: `700`,
+  fontSize: `18px`,
+  letterSpacing: `0px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+});
+
+const ChooseAFileOrDragItHSpan2 = styled("span")({
+  whiteSpace: `pre-wrap`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Inter`,
+  fontWeight: `400`,
+  fontSize: `18px`,
+  letterSpacing: `0px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+});
+
+const ChooseAFileOrDragItH = styled("div")({
+  textAlign: `center`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: "unset",
+  fontWeight: `400`,
+  fontSize: `18px`,
+  letterSpacing: `0px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `623px`,
+  height: `140px`,
+  position: `absolute`,
+  left: `-31px`,
+  top: `111px`,
+});
+
+const Line1 = styled("div")({
+  border: `1px solid rgba(255, 255, 255, 1)`,
+  alignSelf: `stretch`,
+  height: `0px`,
+  margin: `31px 0px 0px 0px`,
+  width: `656px`,
+});
+
+const Frame4 = styled("div")({
+  display: `flex`,
+  position: `relative`,
+  isolation: `isolate`,
+  flexDirection: `column`,
+  justifyContent: `flex-start`,
+  alignItems: `flex-start`,
+  padding: `10px`,
+  boxSizing: `border-box`,
+  alignSelf: `stretch`,
+  margin: `31px 0px 0px 0px`,
+  height: `567px`,
+  width: `656px`,
+});
+
+const Rectangle4 = styled("div")({
+  backgroundColor: `rgba(3, 27, 56, 1)`,
+  alignSelf: `stretch`,
+  height: `547px`,
+  margin: `0px`,
+  width: `636px`,
+});
+
+const LinksThatMatchYourRe = styled("div")({
+  textAlign: `left`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(255, 255, 255, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Inter`,
+  fontWeight: `400`,
+  fontSize: `31px`,
+  letterSpacing: `0px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `319px`,
+  position: `absolute`,
+  left: `788px`,
+  top: `409px`,
+});
+
+const Match = styled("div")({
+  textAlign: `left`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(255, 255, 255, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Inter`,
+  fontWeight: `400`,
+  fontSize: `40px`,
+  letterSpacing: `0px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  position: `absolute`,
+  left: `1197px`,
+  top: `415px`,
+});
+
+const PleaseUploadYourResu = styled("div")({
+  textAlign: `left`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(255, 255, 255, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Inter`,
+  fontWeight: `400`,
+  fontSize: `40px`,
+  letterSpacing: `0px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  position: `absolute`,
+  left: `788px`,
+  top: `511px`,
+});
+
+const PngtreeFileUploadIco = styled("img")({
+  height: `111px`,
+  width: `111px`,
+  objectFit: `cover`,
+  position: `absolute`,
+  left: `1021px`,
+  top: `180px`,
+});
+
+
+function WithoutResume() {
+  return (
+    <WithoutResume1>
+      <Background>
+      </Background>
+      <Frame5>
+        <Scopify>
+          {`Scopify`}
+        </Scopify>
+        <Group2>
+          <Frame2>
+            <ChooseAFileOrDragItH>
+              <ChooseAFileOrDragItHSpan1 >{`Choose a file`}</ChooseAFileOrDragItHSpan1>
+              <ChooseAFileOrDragItHSpan2 >{` or drag it here`}</ChooseAFileOrDragItHSpan2>
+            </ChooseAFileOrDragItH>
+          </Frame2>
+        </Group2>
+        <Line1>
+        </Line1>
+        <Frame4>
+          <Rectangle4>
+          </Rectangle4>
+        </Frame4>
+      </Frame5>
+      <LinksThatMatchYourRe>
+        {`Links That Match Your Resume`}
+      </LinksThatMatchYourRe>
+      <Match>
+        {`Match %`}
+      </Match>
+      <PleaseUploadYourResu>
+        {`Please upload your resume`}
+      </PleaseUploadYourResu>
+      <PngtreeFileUploadIco src={PngtreeFileUploadIcoImage} loading='lazy' alt={"—Pngtree—file upload icon_4646955 1"}/>
+    </WithoutResume1>);
+
+  }
+
+export default WithoutResume;
+
+  
