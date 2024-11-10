191d441d99307554fec0054b6088ea58a02a65ce
Stephanie <sleung32@students.cs.ubc.ca>
2024-11-11 06:05:18 +0800

    file handing and resume parsing

--- service-worker.js
+++ service-worker.js
@@ -4,23 +4,3 @@ const SCOPE = 'https://scope.sciencecoop.ubc.ca/myAccount/co-op/postings.htm';
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
-
-// Listen for messages from content scripts or the popup to open the side panel
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "setSidePanelContent") {
-    let contentPath;
-
-    // Check the message content and decide which HTML to display in the side panel
-    if (message.content === "correct-site") {
-      contentPath = "sidepanel/side_panel.html";  // Path to content for the correct site
-    } else {
-      contentPath = "sidepanel/test.html";  // Path to content for any other site
-    }
-
-    // Update the side panel content
-    chrome.sidePanel.setOptions({
-      path: contentPath,
-      enabled: true  // Enable the side panel with the new content
-    });
-  }
-});
--- sidepanel/side_panel.js
+++ sidepanel/side_panel.js
@@ -1,10 +1,47 @@
+import { apiKey } from './export.js';
+
 const dropArea = document.getElementById("drop-area");
 const inputFile = document.getElementById("input-file");
-const imageView = document.getElementById("img-view");
+const fileList = document.getElementById("file-list")
 
 inputFile.addEventListener("change", uploadFile);
 
 function uploadFile() {
-  let file = URL.createObjectURL(inputFile.files[0]);
-  console.log("file received");
+    let file = inputFile.files[0];
+    console.log("File received.");
+    if (fileList.children.length === 1) {
+        fileList.innerHTML = '';
+    }
+    const fileItem = document.createElement("div");
+    fileItem.classList.add("file-item");
+    fileItem.textContent = `${file.name} • Uploaded`;
+    fileList.appendChild(fileItem);
+
+    const formData = new FormData();
+
+    // Append a file to the form data (example file input from a file input field)
+    formData.append("file_name", file);
+
+
+    // Send the form data using fetch
+    fetch("https://api.superparser.com/parse", {
+        method: "POST",
+        headers: {
+            "X-API-KEY": apiKey
+        },
+        body: formData
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error("Error:", error));
 }
+
+dropArea.addEventListener("dragover", function (e) {
+    e.preventDefault();
+});
+
+dropArea.addEventListener("drop", function (e) {
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    uploadFile();
+});
